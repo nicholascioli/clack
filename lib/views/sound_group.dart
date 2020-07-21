f823068f92759e40cdb56079e17c4a2ae2fba856
Nicholas Cioli <nicholascioli@gmail.com>
2020-07-21 04:30:36 -0500

    add hero animation to music browser list

--- lib/views/sound_group.dart
+++ lib/views/sound_group.dart
@@ -54,95 +54,6 @@ class _SoundGroupState extends State<SoundGroup> {
                 child: SpinKitCubeGrid(color: Colors.black),
               )
             : _buildPage());
-    //   SizedBox(height: 10),
-    //   Row(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     children: [Text("@${_author.uniqueId}", style: userTextStyle)],
-    //   ),
-    //   SizedBox(height: 15),
-    //   Row(
-    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //     children: [
-    //       _buildStatsColumn((v) => v.stats.followingCount, "Following"),
-    //       _buildStatsColumn((v) => v.stats.followerCount, "Followers"),
-    //       _buildStatsColumn((v) => v.stats.heart, "Hearts")
-    //     ],
-    //   ),
-    //   SizedBox(height: 10),
-    //   // TODO: Make this change between 'Follow' and 'Message' / 'IconButton(?)
-    //   //   based on whether the logged in user is following or not.
-    //   ButtonBar(
-    //     alignment: MainAxisAlignment.center,
-    //     children: [
-    //       OutlineButton(
-    //         onPressed: () {},
-    //         child: Text("Message"),
-    //         textColor: Colors.black,
-    //       ),
-    //       OutlineButton.icon(
-    //         onPressed: null,
-    //         icon: Icon(Icons.person_add),
-    //         label: Container(),
-    //         textColor: Colors.black,
-    //       )
-    //     ],
-    //   ),
-    //   Padding(
-    //       padding:
-    //           EdgeInsetsDirectional.only(start: 30, end: 30, top: 15),
-    //       child: Text(
-    //         _author.signature,
-    //         style: softTextStyle,
-    //         textAlign: TextAlign.center,
-    //       )),
-    //   SizedBox(height: 30),
-    // ])),
-    // SliverStickyHeader(
-    //     header: Container(
-    //         decoration: BoxDecoration(
-    //             color: Theme.of(context).canvasColor,
-    //             boxShadow: [
-    //               BoxShadow(
-    //                   color: Colors.grey,
-    //                   blurRadius: 3,
-    //                   offset: Offset(0, 3))
-    //             ]),
-    //         child: TabBar(
-    //           controller: _tabController,
-    //           labelColor: Colors.black,
-    //           unselectedLabelColor: Colors.grey,
-    //           tabs: [
-    //             Tab(icon: Icon(Icons.list)),
-    //             Tab(
-    //                 icon: Stack(
-    //                     alignment: Alignment.bottomRight,
-    //                     children: [
-    //                   Icon(Icons.favorite_border),
-    //                   Padding(
-    //                       padding: EdgeInsets.only(bottom: 3),
-    //                       child: !_author.openFavorite
-    //                           ? Icon(
-    //                               Icons.lock,
-    //                               size: 12,
-    //                             )
-    //                           : null)
-    //                 ]))
-    //           ],
-    //         )),
-    //     // TODO: Allow for this to also be sscrolled together with
-    //     //   its parent. Right now, it scrolls separately :(
-    //     sliver: SliverFillRemaining(
-    //         child: TabBarView(controller: _tabController, children: [
-    //       _buildVideoList(_authorVideos),
-    //       _author.openFavorite
-    //           ? _buildVideoList(_authorFavoritedVideos)
-    //           : Center(
-    //               child: Text(
-    //                   "@${_author.uniqueId} has hidden their liked videos."))
-    //     ])))
-    //     ],
-    //   ),
-    // );
   }
 
   Widget _buildPage() => CustomScrollView(slivers: [
@@ -224,45 +135,47 @@ class _SoundGroupState extends State<SoundGroup> {
                   arguments: VideoFeedArgs(_videos, index, null)),
               child: Container(
                   color: Colors.black,
-                  child: Stack(children: [
-                    AspectRatio(
-                        aspectRatio: 1,
-                        child: FittedBox(
-                            fit: BoxFit.fitWidth,
-                            child: _videos[index] == null
-                                ? Padding(
-                                    padding: EdgeInsets.all(40),
-                                    child:
-                                        SpinKitFadingCube(color: Colors.grey))
-                                : Image.network(_videos[index]
-                                    .video
-                                    .dynamicCover
-                                    .toString()))),
-                    Align(
-                        alignment: Alignment.topLeft,
-                        child: _videos[index] == null ||
-                                !_videos[index].video.isOriginal
-                            ? Container()
-                            : Padding(
-                                padding: EdgeInsets.all(5),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(5),
-                                      topLeft: Radius.circular(5),
-                                      topRight: Radius.circular(20),
-                                      bottomRight: Radius.circular(20)),
-                                  child: Container(
-                                    color: Colors.yellow,
-                                    child: Padding(
-                                        padding: EdgeInsets.only(
-                                            left: 5,
-                                            top: 5,
-                                            bottom: 5,
-                                            right: 10),
-                                        child: Text("Original")),
-                                  ),
-                                )))
-                  ]))))),
+                  child: Hero(
+                      tag: "video_page_$index",
+                      child: Stack(children: [
+                        AspectRatio(
+                            aspectRatio: 1,
+                            child: FittedBox(
+                                fit: BoxFit.fitWidth,
+                                child: _videos[index] == null
+                                    ? Padding(
+                                        padding: EdgeInsets.all(40),
+                                        child: SpinKitFadingCube(
+                                            color: Colors.grey))
+                                    : Image.network(_videos[index]
+                                        .video
+                                        .dynamicCover
+                                        .toString()))),
+                        Align(
+                            alignment: Alignment.topLeft,
+                            child: _videos[index] == null ||
+                                    !_videos[index].video.isOriginal
+                                ? Container()
+                                : Padding(
+                                    padding: EdgeInsets.all(5),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(5),
+                                          topLeft: Radius.circular(5),
+                                          topRight: Radius.circular(20),
+                                          bottomRight: Radius.circular(20)),
+                                      child: Container(
+                                        color: Colors.yellow,
+                                        child: Padding(
+                                            padding: EdgeInsets.only(
+                                                left: 5,
+                                                top: 5,
+                                                bottom: 5,
+                                                right: 10),
+                                            child: Text("Original")),
+                                      ),
+                                    )))
+                      ])))))),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3, crossAxisSpacing: 3, mainAxisSpacing: 3));
 }
