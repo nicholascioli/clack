a71ba3b0e0e7f0877325589bdb94f0b3f54f1d91
Nicholas Cioli <nicholascioli@gmail.com>
2020-08-22 14:15:14 -0500

    lock orientation to portait

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:clack/views/full_image.dart';
 import 'package:clack/views/sign_in_webview.dart';
 import 'package:clack/views/sound_group.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 import 'views/video_feed.dart';
@@ -10,6 +11,9 @@ import 'views/video_feed.dart';
 void main() async {
   // Needed to ensure API can be initialized before app starts
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock orientation to portrait only
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   // Initialize the API
   await API.init();
