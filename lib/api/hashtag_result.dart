afe0e5828800579b687d595920d1ce4b7017f06c
Nicholas Cioli <nicholascioli@gmail.com>
2020-08-26 00:29:56 -0500

    fix forgotten hashtagresult update

--- lib/api/hashtag_result.dart
+++ lib/api/hashtag_result.dart
@@ -1,3 +1,4 @@
+import 'package:clack/api/shared_types.dart';
 import 'package:dataclass/dataclass.dart';
 
 @dataClass
@@ -43,6 +44,9 @@ class HashtagResult {
   /// Stats about the hashtag
   final HashtagStats stats;
 
+  /// Share information for this hashtag
+  final ShareMeta shareMeta;
+
   const HashtagResult(
       {this.id,
       this.title,
@@ -53,17 +57,26 @@ class HashtagResult {
       this.coverThumb,
       this.coverMedium,
       this.coverLarger,
-      this.stats});
+      this.stats,
+      this.shareMeta});
 
-  factory HashtagResult.fromJson(Map<String, dynamic> json) => HashtagResult(
-      id: json["challenge"]["id"],
-      title: json["challenge"]["title"],
-      desc: json["challenge"]["desc"],
-      profileThumb: Uri.parse(json["challenge"]["profileThumb"]),
-      profileMedium: Uri.parse(json["challenge"]["profileMedium"]),
-      profileLarger: Uri.parse(json["challenge"]["profileLarger"]),
-      coverThumb: Uri.parse(json["challenge"]["coverThumb"]),
-      coverMedium: Uri.parse(json["challenge"]["coverMedium"]),
-      coverLarger: Uri.parse(json["challenge"]["coverLarger"]),
-      stats: HashtagStats.fromJson(json["stats"]));
+  factory HashtagResult.fromJson(Map<String, dynamic> raw) {
+    Map<String, dynamic> json = raw["challenge"];
+    Map<String, dynamic> stats = raw["stats"];
+    Map<String, dynamic> share =
+        raw.containsKey("shareMeta") ? raw["shareMeta"] : null;
+
+    return HashtagResult(
+        id: json["id"],
+        title: json["title"],
+        desc: json["desc"],
+        profileThumb: Uri.parse(json["profileThumb"]),
+        profileMedium: Uri.parse(json["profileMedium"]),
+        profileLarger: Uri.parse(json["profileLarger"]),
+        coverThumb: Uri.parse(json["coverThumb"]),
+        coverMedium: Uri.parse(json["coverMedium"]),
+        coverLarger: Uri.parse(json["coverLarger"]),
+        stats: HashtagStats.fromJson(stats),
+        shareMeta: share != null ? ShareMeta.fromJson(share) : null);
+  }
 }
