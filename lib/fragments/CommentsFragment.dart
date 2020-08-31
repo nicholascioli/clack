92daf805497e38073903ffe4e2dc00973c95c460
Nicholas Cioli <nicholascioli@gmail.com>
2020-08-30 20:40:25 -0500

    fix unused import

--- lib/fragments/CommentsFragment.dart
+++ lib/fragments/CommentsFragment.dart
@@ -7,7 +7,6 @@ import 'package:clack/views/user_info.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:intl/intl.dart';
 import 'package:like_button/like_button.dart';
 import 'package:tuple/tuple.dart';
 
